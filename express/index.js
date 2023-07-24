53298023cf83b1ce636b5355e73c11a5b7bf9cc1
Jitendra-Lodhi <jitendra.developer77@gmail.com>
2023-07-24 18:14:57 +0530

    Serc

--- express/index.js
+++ express/index.js
@@ -1,24 +1,46 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const pathdir = path.join(__dirname, "public");
-console.log(pathdir);
-app.use(express.static(pathdir));
-// console.log(express);
+
+// Set the view engine to 'ejs'
+app.set("view engine", "ejs");
+
+// Define the 'views' folder where EJS templates are located
+app.set("views", path.join(__dirname, "views"));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Define route for the root path ("/")
 app.get("/", (req, res) => {
-  res.sendFile(`${pathdir}/index.html`);
+  // Send the 'index.html' file located in the 'public' directory as the response
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+// Define route for the "/about" path
 app.get("/about", (req, res) => {
-  res.sendFile(`${pathdir}/home.html`);
+  // Send the 'home.html' file located in the 'public' directory as the response
+  res.sendFile(path.join(__dirname, "public", "home.html"));
 });
+
+// Define route for the "/data" path
 app.get("/data", (req, res) => {
+  // Send a JSON response back to the client with some data
   res.send({
-    name: `narendra`,
+    name: "narendra",
     age: 23,
     address: "indore",
   });
 });
+
+// Define route for the "/download" path
 app.get("/download", (req, res) => {
-  res.download(`${pathdir}/home.html`);
+  // Trigger a file download of 'home.html' located in the 'public' directory
+  res.download(path.join(__dirname, "public", "home.html"));
 });
-app.listen(3120, console.log("server is running"));
+
+// Start the server and make it listen on port 3000
+app.listen(3000, () => {
+  // This callback function will be executed once the server is successfully listening on the specified port.
+  console.log("Server is running on port 3000");
+});
