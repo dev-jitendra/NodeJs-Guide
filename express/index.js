f621ea8ac46e8971d7ac40d9864dd95deb22b282
Jitendra-Lodhi <jitendra.developer77@gmail.com>
2023-07-24 17:45:38 +0530

    initial commit

--- express/index.js
+++ express/index.js
@@ -1,10 +1,24 @@
-const express =  require('express');
+const express = require("express");
+const path = require("path");
+const app = express();
+const pathdir = path.join(__dirname, "public");
+console.log(pathdir);
+app.use(express.static(pathdir));
 // console.log(express);
-const app=express();
-app.get('/',(req,res)=>{
-    res.send('Hi this is First express rout')
+app.get("/", (req, res) => {
+  res.sendFile(`${pathdir}/index.html`);
 });
-app.get('/about',(req,res)=>{
-    res.send('Hi this is First express rout on about')
+app.get("/about", (req, res) => {
+  res.sendFile(`${pathdir}/home.html`);
 });
-app.listen(3120,(console.log('server is running')));
+app.get("/data", (req, res) => {
+  res.send({
+    name: `narendra`,
+    age: 23,
+    address: "indore",
+  });
+});
+app.get("/download", (req, res) => {
+  res.download(`${pathdir}/home.html`);
+});
+app.listen(3120, console.log("server is running"));
