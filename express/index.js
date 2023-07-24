3f2eb5d7c9ccd42a92ae697b25c15d1bacc2b86c
Jitendra-Lodhi <jitendra.developer77@gmail.com>
2023-07-24 18:58:03 +0530

    ejs update

--- express/index.js
+++ express/index.js
@@ -13,14 +13,24 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Define route for the root path ("/")
 app.get("/", (req, res) => {
+  // Send the 'index.html' file located in the 'views' directory as the response
+  res.render("index", {
+    title: "home",
+  });
+
   // Send the 'index.html' file located in the 'public' directory as the response
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  // res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Define route for the "/about" path
 app.get("/about", (req, res) => {
+  // Send the 'home.html' file located in the 'views' directory as the response
+  res.render("home",{
+    title:"home"
+  });
+
   // Send the 'home.html' file located in the 'public' directory as the response
-  res.sendFile(path.join(__dirname, "public", "home.html"));
+  // res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
 // Define route for the "/data" path
@@ -36,7 +46,7 @@ app.get("/data", (req, res) => {
 // Define route for the "/download" path
 app.get("/download", (req, res) => {
   // Trigger a file download of 'home.html' located in the 'public' directory
-  res.download(path.join(__dirname, "public", "home.html"));
+  // res.download(path.join(__dirname, "public", "home.html"));
 });
 
 // Start the server and make it listen on port 3000
