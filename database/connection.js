f872693b46d77e78749f28d9a36c4c8efcf2c446
Jitendra-Lodhi <jitendra.developer77@gmail.com>
2023-07-25 15:52:48 +0530

    post method

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,11 @@
 const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/nodejsguide")
-  .then(() => {
+mongoose.connect("mongodb://127.0.0.1:27017/nodejsguide").then(() => {
     console.log("Connection is successfully created");
-  })
-  .catch((err) => {
+  }).catch((err) => {
     console.log(`Connection failed ${err}`);
   });
-
 const schema = new mongoose.Schema({
   name: String,
 });
-
 const User = mongoose.model("user", schema);
-
 module.exports = User;
--- database/server.js
+++ database/server.js
@@ -24,7 +24,7 @@ app.get("/user", async (req, res) => {
   }
 });
 
-app.get('/user:id',async(req,res)=>{
+app.get('/user/:id',async(req,res)=>{
     try{
         const id= req.params.id;
         const getiddata= await user.findById({_id:id});
@@ -32,6 +32,16 @@ app.get('/user:id',async(req,res)=>{
     }catch(error){
         res.status(400).send(error);
     }
+});
+
+app.patch('/update/:id',async(req,res)=>{
+    try{
+    const id=req.params.id;
+    const updatedata= await user.findByIdAndUpdate({_id:id},req.body,{new:true});
+    res.send(updatedata);
+}catch(err){
+    res.status(500).send(err);
+}
 })
 app.listen(2300, () => {
   console.log("Server Is Running");
