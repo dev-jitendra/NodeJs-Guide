5ec35db900b45b937428109629c4a2bb9a0c8f38
Jitendra-Lodhi <jitendra.developer77@gmail.com>
2023-07-27 17:37:06 +0530

    Dev-App Setup

---
+++ dev-app/src/components/TextForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function TextForm(props) {
+  return (
+    <>
+      <div>
+        <h1>props.heading</h1>
+        <div class="mb-3">
+          <label for="myBox" class="form-label">
+            Example textarea
+          </label>
+          <textarea class="form-control" id="myBox" rows="8"></textarea>
+        </div>
+      </div>
+    </>
+  );
+}
